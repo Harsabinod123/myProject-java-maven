0065cc8679849aaebb81c9d1651882b9ff880a22
harsabinod <harsa.kjr15@gmail.com>
2026-07-10 14:29:22 +0530

    add all file

---
+++ myProject/src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        String name = "Maven Jenkins";
+
+        System.out.println("Hello " + name);
+        System.out.println("Maven Build Successful");
+    }
+}
